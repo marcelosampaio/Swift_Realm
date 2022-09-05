7740436d6757eb8084fd1726488a1f3bde8e5e75
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-09-05 15:37:02 -0300

    Initial Commit

---
+++ Swift_Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Realm
+//
+//  Created by Marcelo Sampaio on 05/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
