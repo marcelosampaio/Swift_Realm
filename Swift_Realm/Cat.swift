c0f6ed788bb1c4a6f84c3c4f0c85ea3a01b53ef6
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-09-19 20:15:07 -0300

    Working with Realm schema version ✅

--- Swift_Realm/Cat.swift
+++ Swift_Realm/Cat.swift
@@ -13,4 +13,6 @@ class Cat: Object {
     @objc dynamic var name: String?
     @objc dynamic var color: String?
     @objc dynamic var gender: String?
+    @objc dynamic var health: String?
+    @objc dynamic var pending: String?
 }
--- Swift_Realm/ViewController.swift
+++ Swift_Realm/ViewController.swift
@@ -11,19 +11,23 @@ import RealmSwift
 
 class ViewController: UIViewController {
     
-    private let realm = try! Realm()
+    let schemaVersion: Int = 2
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("📍 Realm path: \(path())")
 //        add()
-        
+//
         readAll()
         
     }
     
     func path() -> String {
+        let config = Realm.Configuration(
+            schemaVersion: UInt64(schemaVersion))
+        Realm.Configuration.defaultConfiguration = config
+        let realm = try! Realm()
         let realmPath: String = realm.configuration.fileURL?.absoluteString ?? ""
         return realmPath
     }
@@ -32,19 +36,29 @@ class ViewController: UIViewController {
     func add() {
         
         let cat = Cat()
-        cat.name = "Martina"
-        cat.gender = "Female"
-        cat.color = "White"
+        cat.name = "Third"
+        cat.gender = "Male"
+        cat.color = "Brown"
+        cat.health = "needsAttention"
         
+        let config = Realm.Configuration(
+            schemaVersion: UInt64(schemaVersion))
+        let realm = try! Realm()
+        Realm.Configuration.defaultConfiguration = config
         try! realm.write {
             realm.add(cat)
         }
     }
     
     func readAll() {
+        let config = Realm.Configuration(
+            schemaVersion: UInt64(schemaVersion))
+        
+        let realm = try! Realm()
         let cats = realm.objects(Cat.self)
         
         for cat in cats {
+            
             print("👉 This is cat \(cat.name ?? "***")")
         }
         
